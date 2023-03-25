b1bd607140cdd93a6797b3942be85a2d470b4a04
DadaLV <vynohradova.l.a@gmail.com>
2023-03-25 02:46:28 +0100

    add todo app components

---
+++ todo-app-v1/src/components/Todos/Todo.js
@@ -0,0 +1,12 @@
+import styles from "./Todo.module.css";
+
+function Todo() {
+  return (
+    <div>
+      <img src="" alt="" />
+      <p>todo</p>
+    </div>
+  );
+}
+
+export default Todo;
---
+++ todo-app-v1/src/components/Todos/TodoList.js
@@ -0,0 +1,14 @@
+import Todo from "./Todo";
+import styles from "./TodoList.module.css";
+
+function TodoList() {
+  return (
+    <>
+      <Todo />
+      <Todo />
+      <Todo />
+    </>
+  );
+}
+
+export default TodoList;
