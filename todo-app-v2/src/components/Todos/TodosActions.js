c7743b9eb4e2d56b4aee4e81cf75d8e4f0aa29ea
DadaLV <vynohradova.l.a@gmail.com>
2023-03-26 04:18:08 +0200

    add buttons

--- todo-app-v2/src/components/Todos/TodosActions.js
+++ todo-app-v2/src/components/Todos/TodosActions.js
@@ -1,10 +1,18 @@
-import styles from "./TodosActions.module.css"
+import { RiDeleteBin2Line, RiRefreshLine } from "react-icons/ri";
+import styles from "./TodosActions.module.css";
+import Button from "../UI/Button";
 
 function TodosActions() {
-  <>
-    <button>Reset</button>
-    <button>Delete</button>
-  </>
+  return (
+    <>
+      <Button title="Reset Todos">
+        <RiRefreshLine />
+      </Button>
+      <Button title="Clear Complited Todos">
+        <RiDeleteBin2Line />
+      </Button>
+    </>
+  );
 }
 
-export default TodosActions
+export default TodosActions;
